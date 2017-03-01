de78554ba9f8a67722bfe7a27ea0d23440357d52
Jurnell Cockhren <jurnell@sophicware.com>
2017-02-28 17:31:14 -0800

    Fake Trello Exercise Boilerplate

---
+++ FakeTrello/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FakeTrello.Startup))]
+namespace FakeTrello
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
