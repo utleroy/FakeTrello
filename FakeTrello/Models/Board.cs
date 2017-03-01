7ea89984f554d452b5857f58b417596d4f953d72
Jurnell Cockhren <jurnell@sophicware.com>
2017-02-28 19:16:09 -0800

    In class demonstration: First pass of building Entity models and making simple relationships

---
+++ FakeTrello/Models/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Board
+    {
+        [Key]
+        public int BoardId { get; set; }
+
+        public string Name { get; set; }
+
+        public string URL { get; set; }
+    }
+}
