9c7a7ea4c430749e05042ec593445f2f6e091a69
Jurnell Cockhren <jurnell@sophicware.com>
2017-03-04 08:46:01 -0800

    Added Navigation properties for List and TrelloUser/Owner to Board model. New List model for Trello Lists.

---
+++ FakeTrello/Models/List.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FakeTrello.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListId {get;set;}
+
+        public string Name { get; set; }
+
+        public List<Card> Cards { get; set; }
+    }
+}
