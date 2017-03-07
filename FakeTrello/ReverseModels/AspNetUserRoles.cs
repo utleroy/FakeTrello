27360c87f2c22389b24f57044947ce85caea4b85
Jurnell Cockhren <jurnell@sophicware.com>
2017-03-06 17:32:49 -0800

    In-Class Interpretation of InitialCreate migration

---
+++ FakeTrello/ReverseModels/AspNetUserRoles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.ReverseModels
+{
+    // This really doesn't exist, but the relationship does. ;)
+    public class AspNetUserRoles
+    {
+        [Key]
+        [MaxLength(128)]
+        public string UserId { get; set; }
+
+        [Key]
+        [MaxLength(128)]
+        public string RoleId { get; set; }
+    }
+}
