e716dded15fb66516761cee8df398f3b33fce11d
Jurnell Cockhren <jurnell@sophicware.com>
2017-03-11 12:01:51 -0800

    New Interface ehlping to flesh out methods for features

---
+++ FakeTrello/DAL/IRepository.cs
@@ -0,0 +1,40 @@
+﻿using FakeTrello.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeTrello.DAL
+{
+    public interface IRepository
+    {
+        // List of methods to help deliver features
+        // Create
+        void AddBoard(string name, ApplicationUser owner);
+        void AddList(string name, Board board);
+        void AddList(string name, int boardId);
+        void AddCard(string name, List list, ApplicationUser owner);
+        void AddCard(string name, int listId, string ownerId);
+
+        // Read
+        List<Card> GetCardsFromList(int listId);
+        List<Card> GetCardsFromBoard(int boardId);
+        Card GetCard(int cardId);
+        List GetList(int listId);
+        List<List> GetListsFromBoard(int boardId); // List of Trello Lists
+        List<Board> GetBoardsFromUser(string userId);
+        Board GetBoard(int boardId);
+        List<ApplicationUser> GetCardAttendees(int cardId); 
+
+        // Update
+        bool AttachUser(string userId, int cardId); // true: successful, false: not successful
+        bool MoveCard(int cardId, int oldListId, int newListId);
+        bool CopyCard(int cardId, int newListId, string newOwnerId);
+
+        // Delete
+        bool RemoveBoard(int boardId);
+        bool RemoveList(int listId);
+        bool RemoveCard(int cardId);
+    }
+}
