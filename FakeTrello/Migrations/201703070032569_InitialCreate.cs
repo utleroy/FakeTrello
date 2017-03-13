297d56a4987c08edf99d9b5258c41d0917873da8
Jurnell Cockhren <jurnell@sophicware.com>
2017-03-13 16:29:29 -0700

    Updated InitialCreate Migration to use Entry DBSets for Entity.

--- FakeTrello/Migrations/201703070032569_InitialCreate.cs
+++ FakeTrello/Migrations/201703070032569_InitialCreate.cs
@@ -8,27 +8,57 @@ namespace FakeTrello.Migrations
         public override void Up()
         {
             CreateTable(
-                "dbo.AspNetRoles",
+                "dbo.Boards",
                 c => new
                     {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Name = c.String(nullable: false, maxLength: 256),
+                        BoardId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        URL = c.String(),
+                        Owner_Id = c.String(maxLength: 128),
                     })
-                .PrimaryKey(t => t.Id)
-                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+                .PrimaryKey(t => t.BoardId)
+                .ForeignKey("dbo.AspNetUsers", t => t.Owner_Id)
+                .Index(t => t.Owner_Id);
             
             CreateTable(
-                "dbo.AspNetUserRoles",
+                "dbo.Lists",
                 c => new
                     {
-                        UserId = c.String(nullable: false, maxLength: 128),
-                        RoleId = c.String(nullable: false, maxLength: 128),
+                        ListId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Board_BoardId = c.Int(),
                     })
-                .PrimaryKey(t => new { t.UserId, t.RoleId })
-                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
-                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
-                .Index(t => t.UserId)
-                .Index(t => t.RoleId);
+                .PrimaryKey(t => t.ListId)
+                .ForeignKey("dbo.Boards", t => t.Board_BoardId)
+                .Index(t => t.Board_BoardId);
+            
+            CreateTable(
+                "dbo.Cards",
+                c => new
+                    {
+                        CardId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        BelongsTo_ListId = c.Int(),
+                        Owner_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.CardId)
+                .ForeignKey("dbo.Lists", t => t.BelongsTo_ListId)
+                .ForeignKey("dbo.AspNetUsers", t => t.Owner_Id)
+                .Index(t => t.BelongsTo_ListId)
+                .Index(t => t.Owner_Id);
+            
+            CreateTable(
+                "dbo.Collaborators",
+                c => new
+                    {
+                        CollaboratorId = c.Int(nullable: false, identity: true),
+                        ApplicationUserId = c.String(nullable: false),
+                        CardId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CollaboratorId)
+                .ForeignKey("dbo.Cards", t => t.CardId, cascadeDelete: true)
+                .Index(t => t.CardId);
             
             CreateTable(
                 "dbo.AspNetUsers",
@@ -75,25 +105,62 @@ namespace FakeTrello.Migrations
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId);
             
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
         }
         
         public override void Down()
         {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Boards", "Owner_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Lists", "Board_BoardId", "dbo.Boards");
+            DropForeignKey("dbo.Cards", "Owner_Id", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
-            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Collaborators", "CardId", "dbo.Cards");
+            DropForeignKey("dbo.Cards", "BelongsTo_ListId", "dbo.Lists");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
             DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
             DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
             DropIndex("dbo.AspNetUsers", "UserNameIndex");
-            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
-            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
-            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Collaborators", new[] { "CardId" });
+            DropIndex("dbo.Cards", new[] { "Owner_Id" });
+            DropIndex("dbo.Cards", new[] { "BelongsTo_ListId" });
+            DropIndex("dbo.Lists", new[] { "Board_BoardId" });
+            DropIndex("dbo.Boards", new[] { "Owner_Id" });
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.AspNetUserRoles");
             DropTable("dbo.AspNetUserLogins");
             DropTable("dbo.AspNetUserClaims");
             DropTable("dbo.AspNetUsers");
-            DropTable("dbo.AspNetUserRoles");
-            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Collaborators");
+            DropTable("dbo.Cards");
+            DropTable("dbo.Lists");
+            DropTable("dbo.Boards");
         }
     }
 }
