b3ed0e150f7d75d5339fa222e33797da5e713ec1
Jurnell Cockhren <jurnell@sophicware.com>
2017-03-13 16:30:06 -0700

    Added Repository concrete class for feature implementation.

---
+++ FakeTrello/DAL/FakeTrelloRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FakeTrello.Models;
+
+namespace FakeTrello.DAL
+{
+    public class FakeTrelloRepository : IRepository
+    {
+        public void AddBoard(string name, ApplicationUser owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCard(string name, int listId, string ownerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCard(string name, List list, ApplicationUser owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddList(string name, int boardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddList(string name, Board board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AttachUser(string userId, int cardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CopyCard(int cardId, int newListId, string newOwnerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Board GetBoard(int boardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Board> GetBoardsFromUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card GetCard(int cardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ApplicationUser> GetCardAttendees(int cardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Card> GetCardsFromBoard(int boardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Card> GetCardsFromList(int listId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List GetList(int listId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List> GetListsFromBoard(int boardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveCard(int cardId, int oldListId, int newListId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveBoard(int boardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveCard(int cardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveList(int listId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
