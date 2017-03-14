8651cf53a66abef0ccfa5169c3b74a53f9eae276
Lee Hankins <bryanhankins@me.com>
2017-03-13 18:12:25 -0700

    mock start on mocking trello

---
+++ FakeTrello.Tests/FakeTrellRepository.cs
@@ -0,0 +1,14 @@
+﻿using FakeTrello.DAL;
+
+namespace FakeTrello.Tests
+{
+    public class FakeTrelloRepository : IRepository
+    {
+        public FakeTrelloContext Context { get; set; }
+
+        public FakeTrelloRepository()
+        {
+
+        }
+    }
+}
---
+++ FakeTrello.Tests/FakeTrelloRepoTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeTrello.DAL;
+using FakeTrello.Tests;
+
+namespace FakeTrello.Tests
+{
+    [TestClass]
+    public class FakeTrelloRepoTest
+    {
+        [TestMethod]
+        public void EnsureCanCreate()
+        {
+            FakeTrelloRepository repo = new FakeTrelloRepository();
+
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void EnsureCanInject()
+        {
+            FakeTrelloContext context = new FakeTrelloContext();
+            FakeTrelloRepository repo = new FakeTrelloRepository();
+
+            Assert.IsNotNull(repo.Context);
+
+        }
+    }
+}
