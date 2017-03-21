53378221afc5176247f97baeba475e8a7274b1fa
Lee Hankins <bryanhankins@me.com>
2017-03-20 19:12:55 -0700

    implemented DbConnection and sql statements

--- FakeTrello.Tests/DAL/FakeTrelloRepoTests.cs
+++ FakeTrello.Tests/DAL/FakeTrelloRepoTests.cs
@@ -9,175 +9,175 @@ using System.Collections.Generic;
 
 namespace FakeTrello.Tests.DAL
 {
-    [TestClass]
-    public class FakeTrelloRepoTests
-    {
-        public Mock<FakeTrelloContext> fake_context { get; set; }
-        public FakeTrelloRepository repo { get; set; }
-        public Mock<DbSet<Board>> mock_boards_set { get; set; }
-        public IQueryable<Board> query_boards { get; set; }
-        public List<Board> fake_board_table { get; set; }
-        public ApplicationUser sally { get; set; }
-        public ApplicationUser sammy { get; set; }
+    //[TestClass]
+    //public class FakeTrelloRepoTests
+    //{
+    //    public Mock<FakeTrelloContext> fake_context { get; set; }
+    //    public FakeTrelloRepository repo { get; set; }
+    //    public Mock<DbSet<Board>> mock_boards_set { get; set; }
+    //    public IQueryable<Board> query_boards { get; set; }
+    //    public List<Board> fake_board_table { get; set; }
+    //    public ApplicationUser sally { get; set; }
+    //    public ApplicationUser sammy { get; set; }
 
-        [TestInitialize]
-        public void Setup()
-        {
-            fake_board_table = new List<Board>();
-            fake_context = new Mock<FakeTrelloContext>();
-            mock_boards_set = new Mock<DbSet<Board>>();
-            repo = new FakeTrelloRepository(fake_context.Object);
+    //    [TestInitialize]
+    //    public void Setup()
+    //    {
+    //        fake_board_table = new List<Board>();
+    //        fake_context = new Mock<FakeTrelloContext>();
+    //        mock_boards_set = new Mock<DbSet<Board>>();
+    //        repo = new FakeTrelloRepository(fake_context.Object);
 
-            sally = new ApplicationUser { Id = "sally-id-1" };
-            sammy = new ApplicationUser { Id = "sammy-id-1" };
-        }
+    //        sally = new ApplicationUser { Id = "sally-id-1" };
+    //        sammy = new ApplicationUser { Id = "sammy-id-1" };
+    //    }
 
-        public void CreateFakeDatabase()
-        {
-            query_boards = fake_board_table.AsQueryable(); // Re-express this list as something that understands queries
+    //    public void CreateFakeDatabase()
+    //    {
+    //        query_boards = fake_board_table.AsQueryable(); // Re-express this list as something that understands queries
             
-            // Hey LINQ, use the Provider from our *cough* fake *cough* board table/list
-            mock_boards_set.As<IQueryable<Board>>().Setup(b => b.Provider).Returns(query_boards.Provider);
-            mock_boards_set.As<IQueryable<Board>>().Setup(b => b.Expression).Returns(query_boards.Expression);
-            mock_boards_set.As<IQueryable<Board>>().Setup(b => b.ElementType).Returns(query_boards.ElementType);
-            mock_boards_set.As<IQueryable<Board>>().Setup(b => b.GetEnumerator()).Returns(() => query_boards.GetEnumerator());
+    //        // Hey LINQ, use the Provider from our *cough* fake *cough* board table/list
+    //        mock_boards_set.As<IQueryable<Board>>().Setup(b => b.Provider).Returns(query_boards.Provider);
+    //        mock_boards_set.As<IQueryable<Board>>().Setup(b => b.Expression).Returns(query_boards.Expression);
+    //        mock_boards_set.As<IQueryable<Board>>().Setup(b => b.ElementType).Returns(query_boards.ElementType);
+    //        mock_boards_set.As<IQueryable<Board>>().Setup(b => b.GetEnumerator()).Returns(() => query_boards.GetEnumerator());
 
-            mock_boards_set.Setup(b => b.Add(It.IsAny<Board>())).Callback((Board board) => fake_board_table.Add(board));
+    //        mock_boards_set.Setup(b => b.Add(It.IsAny<Board>())).Callback((Board board) => fake_board_table.Add(board));
 
-            mock_boards_set.Setup(b => b.Remove(It.IsAny<Board>())).Callback((Board board) => fake_board_table.Remove(board));
+    //        mock_boards_set.Setup(b => b.Remove(It.IsAny<Board>())).Callback((Board board) => fake_board_table.Remove(board));
 
-            fake_context.Setup(c => c.Boards).Returns(mock_boards_set.Object); // Context.Boards returns fake_board_table (a list)
-            fake_context.Setup(c => c.SaveChanges()).Returns(0).Verifiable();
-        }
+    //        fake_context.Setup(c => c.Boards).Returns(mock_boards_set.Object); // Context.Boards returns fake_board_table (a list)
+    //        fake_context.Setup(c => c.SaveChanges()).Returns(0).Verifiable();
+    //    }
 
-        [TestMethod]
-        public void EnsureICanCreateInstanceofRepo()
-        {
-            FakeTrelloRepository repo = new FakeTrelloRepository();
+    //    [TestMethod]
+    //    public void EnsureICanCreateInstanceofRepo()
+    //    {
+    //        FakeTrelloRepository repo = new FakeTrelloRepository();
 
-            Assert.IsNotNull(repo);
-        }
+    //        Assert.IsNotNull(repo);
+    //    }
 
-        [TestMethod]
-        public void EnsureIHaveNotNullContext()
-        {
-            FakeTrelloRepository repo = new FakeTrelloRepository();
+    //    [TestMethod]
+    //    public void EnsureIHaveNotNullContext()
+    //    {
+    //        FakeTrelloRepository repo = new FakeTrelloRepository();
 
-            Assert.IsNotNull(repo.Context);
-        }
+    //        Assert.IsNotNull(repo.Context);
+    //    }
 
-        [TestMethod]
-        public void EnsureICanInjectContextInstance()
-        {
-            FakeTrelloContext context = new FakeTrelloContext();
-            FakeTrelloRepository repo = new FakeTrelloRepository(context);
+    //    [TestMethod]
+    //    public void EnsureICanInjectContextInstance()
+    //    {
+    //        FakeTrelloContext context = new FakeTrelloContext();
+    //        FakeTrelloRepository repo = new FakeTrelloRepository(context);
 
-            Assert.IsNotNull(repo.Context);
-        }
+    //        Assert.IsNotNull(repo.Context);
+    //    }
 
-        [TestMethod]
-        public void EnsureICanAddBoard()
-        {
-            // Arrange
-            CreateFakeDatabase();
+    //    [TestMethod]
+    //    public void EnsureICanAddBoard()
+    //    {
+    //        // Arrange
+    //        CreateFakeDatabase();
 
-            ApplicationUser a_user = new ApplicationUser {
-                Id = "my-user-id",
-                UserName = "Sammy",
-                Email = "sammy@gmail.com"
-            };
+    //        ApplicationUser a_user = new ApplicationUser {
+    //            Id = "my-user-id",
+    //            UserName = "Sammy",
+    //            Email = "sammy@gmail.com"
+    //        };
 
-            // Act
-            repo.AddBoard("My Board", a_user);
+    //        // Act
+    //        repo.AddBoard("My Board", a_user);
 
-            // Assert
-            Assert.AreEqual(1, repo.Context.Boards.Count());
-        }
+    //        // Assert
+    //        Assert.AreEqual(1, repo.Context.Boards.Count());
+    //    }
 
-        [TestMethod]
-        public void EnsureICanReturnBoards()
-        {
-            // Arrange
-            fake_board_table.Add(new Board { Name = "My Board" });
-            CreateFakeDatabase();
+    //    [TestMethod]
+    //    public void EnsureICanReturnBoards()
+    //    {
+    //        // Arrange
+    //        fake_board_table.Add(new Board { Name = "My Board" });
+    //        CreateFakeDatabase();
 
-            // Act
-            int expected_board_count = 1;
-            int actual_board_count = repo.Context.Boards.Count();
+    //        // Act
+    //        int expected_board_count = 1;
+    //        int actual_board_count = repo.Context.Boards.Count();
 
-            // Assert
-            Assert.AreEqual(expected_board_count, actual_board_count);
-        }
+    //        // Assert
+    //        Assert.AreEqual(expected_board_count, actual_board_count);
+    //    }
 
-        [TestMethod]
-        public void EnsureICanFindABoard()
-        {
-            // Arrange
-            fake_board_table.Add(new Board { BoardId = 1, Name = "My Board" });
-            CreateFakeDatabase();
+    //    [TestMethod]
+    //    public void EnsureICanFindABoard()
+    //    {
+    //        // Arrange
+    //        fake_board_table.Add(new Board { BoardId = 1, Name = "My Board" });
+    //        CreateFakeDatabase();
 
-            // Act
-            string expected_board_name = "My Board";
-            Board actual_board = repo.GetBoard(1);
-            string actual_board_name = repo.GetBoard(1).Name;
+    //        // Act
+    //        string expected_board_name = "My Board";
+    //        Board actual_board = repo.GetBoard(1);
+    //        string actual_board_name = repo.GetBoard(1).Name;
 
-            // Assert
-            Assert.IsNotNull(actual_board);
-            Assert.AreEqual(expected_board_name, actual_board_name);
-        }
+    //        // Assert
+    //        Assert.IsNotNull(actual_board);
+    //        Assert.AreEqual(expected_board_name, actual_board_name);
+    //    }
 
-        [TestMethod]
-        public void EnsureICanGetUserBoards()
-        {
-            // Arrange
-            fake_board_table.Add(new Board { BoardId = 1, Name = "My Board", Owner = sally });
-            fake_board_table.Add(new Board { BoardId = 2, Name = "My Board", Owner = sally });
-            fake_board_table.Add(new Board { BoardId = 3, Name = "My Board", Owner = sammy });
-            CreateFakeDatabase();
+    //    [TestMethod]
+    //    public void EnsureICanGetUserBoards()
+    //    {
+    //        // Arrange
+    //        fake_board_table.Add(new Board { BoardId = 1, Name = "My Board", Owner = sally });
+    //        fake_board_table.Add(new Board { BoardId = 2, Name = "My Board", Owner = sally });
+    //        fake_board_table.Add(new Board { BoardId = 3, Name = "My Board", Owner = sammy });
+    //        CreateFakeDatabase();
 
-            // Act
-            int expected_board_count = 2;
-            int actual_board_count = repo.GetBoardsFromUser(sally.Id).Count;
+    //        // Act
+    //        int expected_board_count = 2;
+    //        int actual_board_count = repo.GetBoardsFromUser(sally.Id).Count;
 
-            // Assert
-            Assert.AreEqual(expected_board_count, actual_board_count);
-        }
+    //        // Assert
+    //        Assert.AreEqual(expected_board_count, actual_board_count);
+    //    }
 
-        [TestMethod]
-        public void EnsureICanRemoveBoard()
-        {
-            // Arrange
-            fake_board_table.Add(new Board { BoardId = 1, Name = "My Board", Owner = sally });
-            fake_board_table.Add(new Board { BoardId = 2, Name = "My Board", Owner = sally });
-            fake_board_table.Add(new Board { BoardId = 3, Name = "My Board", Owner = sammy });
-            CreateFakeDatabase();
+    //    [TestMethod]
+    //    public void EnsureICanRemoveBoard()
+    //    {
+    //        // Arrange
+    //        fake_board_table.Add(new Board { BoardId = 1, Name = "My Board", Owner = sally });
+    //        fake_board_table.Add(new Board { BoardId = 2, Name = "My Board", Owner = sally });
+    //        fake_board_table.Add(new Board { BoardId = 3, Name = "My Board", Owner = sammy });
+    //        CreateFakeDatabase();
 
-            // Act
-            int expected_board_count = 2;
-            repo.RemoveBoard(3);
-            int actual_board_count = repo.Context.Boards.Count();
+    //        // Act
+    //        int expected_board_count = 2;
+    //        repo.RemoveBoard(3);
+    //        int actual_board_count = repo.Context.Boards.Count();
 
-            // Assert
-            Assert.AreEqual(expected_board_count, actual_board_count);
+    //        // Assert
+    //        Assert.AreEqual(expected_board_count, actual_board_count);
 
-        }
-        [TestMethod]
-        public void EnsureICanEditName()
-        {
-            // Arrange
-            fake_board_table.Add(new Board { BoardId = 1, Name = "My Board", Owner = sally });
-            CreateFakeDatabase();
+    //    }
+    //    [TestMethod]
+    //    public void EnsureICanEditName()
+    //    {
+    //        // Arrange
+    //        fake_board_table.Add(new Board { BoardId = 1, Name = "My Board", Owner = sally });
+    //        CreateFakeDatabase();
 
-            // Act
-            string expected_board_name = "Our Board";
-            repo.EditBoardName(1, expected_board_name);
-            string actual_board_name = repo.GetBoard(1).Name;
+    //        // Act
+    //        string expected_board_name = "Our Board";
+    //        repo.EditBoardName(1, expected_board_name);
+    //        string actual_board_name = repo.GetBoard(1).Name;
 
-            // Assert
-            Assert.AreEqual(expected_board_name, actual_board_name);
-            fake_context.Verify(c => c.SaveChanges(), Times.Once());
+    //        // Assert
+    //        Assert.AreEqual(expected_board_name, actual_board_name);
+    //        fake_context.Verify(c => c.SaveChanges(), Times.Once());
             
-        }
+    //    }
 
-    }
+    //}
 }
--- FakeTrello/DAL/FakeTrelloRepository.cs
+++ FakeTrello/DAL/FakeTrelloRepository.cs
@@ -3,31 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using FakeTrello.Models;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
 
 namespace FakeTrello.DAL
 {
     public class FakeTrelloRepository : IRepository
     {
 
-        public FakeTrelloContext Context { get; set; }
+       // public FakeTrelloContext Context { get; set; }
         //private FakeTrelloContext context; // Data member
+        SqlConnection _trelloConnection;
 
         public FakeTrelloRepository()
         {
-            Context = new FakeTrelloContext();
+            _trelloConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
         }
-
-        public FakeTrelloRepository(FakeTrelloContext context)
-        {
-            Context = context;
-        }
-
 
         public void AddBoard(string name, ApplicationUser owner)
         {
             Board board = new Board { Name = name, Owner = owner };
-            Context.Boards.Add(board);
-            Context.SaveChanges();
+            //Context.Boards.Add(board);
+            //Context.SaveChanges();
+
+                _trelloConnection.Open();
+            try
+            {
+                var addBoardCommand = _trelloConnection.CreateCommand();
+                addBoardCommand.CommandText = $" Insert into Boards(Name, Owner_Id) values(@name, @ownerId)";
+
+                var nameParameter = new SqlParameter("name", SqlDbType.VarChar);
+                nameParameter.Value = name;
+                addBoardCommand.Parameters.Add(nameParameter);
+
+                var ownerParameter = new SqlParameter("name", SqlDbType.Int);
+                ownerParameter.Value = name;
+                addBoardCommand.Parameters.Add(ownerParameter);
+
+                addBoardCommand.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+            _trelloConnection.Close();
+            }
         }
 
         public void AddCard(string name, int listId, string ownerId)
@@ -63,22 +89,89 @@ namespace FakeTrello.DAL
         public Board GetBoard(int boardId)
         {
             // SELECT * FROM Boards WHERE BoardId == boardId 
-            Board found_board = Context.Boards.FirstOrDefault(b => b.BoardId == boardId); // returns null if nothing is found
-            return found_board;
 
-            /* Using .First() throws an exception if nothing is found
-             * try {
-             * Board found_board = Context.Boards.First(b => b.BoardId == boardId); 
-             * return found_board;
-             * } catch(Exception e) {
-             * return null;
-             * }
-             */
+            _trelloConnection.Open();
+
+            try
+            {
+                var getBoardCommand = _trelloConnection.CreateCommand();
+                getBoardCommand.CommandText = @"
+                SELECT boardId,Name<Url,Owner_Id 
+                FROM Boards 
+                WHERE BoardId == @boardId";
+                var boardIdParameter = new SqlParameter("boardId", SqlDbType.Int);
+                boardIdParameter.Value = boardId;
+                getBoardCommand.Parameters.Add(boardIdParameter);
+
+                var reader = getBoardCommand.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    var board = new Board
+                    {
+                        BoardId = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        URL = reader.GetString(2),
+                        Owner = new ApplicationUser { Id = reader.GetString(3) }
+                    };
+                    return board;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _trelloConnection.Close();
+            }
+           
+             return null;
+            
         }
 
         public List<Board> GetBoardsFromUser(string userId)
         {
-            return Context.Boards.Where(b => b.Owner.Id == userId).ToList();
+            _trelloConnection.Open();
+
+            try
+            {
+                var getBoardCommand = _trelloConnection.CreateCommand();
+                getBoardCommand.CommandText = @"
+                SELECT boardId,Name<Url,Owner_Id 
+                FROM Boards 
+                WHERE BoardId == @userId";
+                var boardIdParameter = new SqlParameter("userId", SqlDbType.VarChar);
+                boardIdParameter.Value = userId;
+                getBoardCommand.Parameters.Add(boardIdParameter);
+
+                var reader = getBoardCommand.ExecuteReader();
+
+
+                var boards = new List<Board>();
+                while (reader.Read())
+                {
+                    var board = new Board
+                    {
+                        BoardId = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        URL = reader.GetString(2),
+                        Owner = new ApplicationUser { Id = reader.GetString(3) }
+                    };
+                    boards.Add(board);
+                }
+                return boards;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _trelloConnection.Close();
+            }
+
+            return new List<Board>();
         }
 
         public Card GetCard(int cardId)
@@ -118,15 +211,35 @@ namespace FakeTrello.DAL
 
         public bool RemoveBoard(int boardId)
         {
-            Board found_board = GetBoard(boardId);
-            if (found_board != null)
+            _trelloConnection.Open();
+
+            try
             {
-                Context.Boards.Remove(found_board);
-                Context.SaveChanges();
+                var removeBoardCommand = _trelloConnection.CreateCommand();
+                removeBoardCommand.CommandText = @"
+                    Delete
+                    From Boards
+                    Where boardId = @boardId";
+
+                var boardIdParameter = new SqlParameter("boardId", SqlDbType.Int);
+                boardIdParameter.Value = boardId;
+                removeBoardCommand.Parameters.Add(boardIdParameter);
+
+                var reader = removeBoardCommand.ExecuteNonQuery();
+
                 return true;
             }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                _trelloConnection.Close();
+            }
+
             return false;
-            
         }
 
         public bool RemoveCard(int cardId)
@@ -141,13 +254,34 @@ namespace FakeTrello.DAL
 
         public void EditBoardName(int boardId, string newname)
         {
-            Board found_board = GetBoard(boardId);
-            if(found_board != null)
+            _trelloConnection.Open();
+            try
             {
-                found_board.Name = newname; // Akin to 'git add'
-                Context.SaveChanges(); // Akin to 'git commit'
+                var updateBoardCommand = _trelloConnection.CreateCommand();
+                updateBoardCommand.CommandText = @"
+                    Update Boards
+                    Set Name = @NewName
+                    Where boardId = @boardId";
+
+                var nameParameter = new SqlParameter("NewName", SqlDbType.VarChar);
+                nameParameter.Value = newname;
+                updateBoardCommand.Parameters.Add(nameParameter);
+
+                var boardIdParameter = new SqlParameter("name", SqlDbType.Int);
+                boardIdParameter.Value = boardId;
+                updateBoardCommand.Parameters.Add(boardIdParameter);
+
+                updateBoardCommand.ExecuteNonQuery();
             }
-            // False Positive: SaveChanges is missing.
-        }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                _trelloConnection.Close();
+            }
+         }
     }
 }
